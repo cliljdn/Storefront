6b45c9c6c19b2e6b9f0a84eeab4526ed939e04f2
cliljdn <caliljaudiannn@gmail.com>
2021-06-08 04:47:45 +0800

    account endpoint

--- Controllers/Classes/Account.js
+++ Controllers/Classes/Account.js
@@ -1,4 +1,6 @@
 const AccountModel = require('../../Models/Schemas/AccountSchema')
+const bcrypt = require('bcrypt')
+const jwt = require('../../helpers/token/jwt')
 
 module.exports = class Account {
      constructor(obj) {
@@ -19,7 +21,32 @@ module.exports = class Account {
           return AccountModel.find({})
      }
 
-     static insertAccount(obj) {
+     static async insertAccount(obj) {
+          const ifExist = await AccountModel.findOne({ email: obj.email })
+
+          if (!ifExist) throw new Error('Email Already Exist')
+
+          obj.password = await bcrypt.hashSync(obj.password, 10)
           AccountModel.create(obj)
+     }
+
+     static async loginUser(obj) {
+          const ifExist = await AccountModel.findOne({ email: obj.email })
+
+          if (!ifExist) throw new Error('Account Does not Exist')
+
+          let isPasswordCorrect = await bcrypt.compareSync(
+               obj.password,
+               ifExist.password
+          )
+
+          if (!isPasswordCorrect) throw new Error('Wrong Password')
+
+          let token = await jwt.sign({ id: ifExist._id })
+          return {
+               token: token,
+               name: `${ifExist.account_type}-${ifExist._id}`,
+               auth: true,
+          }
      }
 }
--- Controllers/Endpoints/AccountsEndpoints.js
+++ Controllers/Endpoints/AccountsEndpoints.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const AccountModel = require('../Classes/Account')
 const router = express.Router()
-const bcrypt = require('bcrypt')
-router.get('/register/account', async (req, res, next) => {
-     try {
-          const data = await AccountModel.listAccounts()
 
-          res.status(200).json(data)
+router.post('/register/account', async (req, res, next) => {
+     const { email, password, account_type } = req.body
+     try {
+          // let account = new AccountModel
+
+          await AccountModel.insertAccount(req.body)
+
+          res.status(200).json('good')
+     } catch (error) {
+          next(error)
+     }
+})
+
+router.post('/login/account', async (req, res, next) => {
+     try {
+          // let account = new AccountModel
+
+          let auth = await AccountModel.loginUser(req.body)
+
+          res.status(200).json(auth)
      } catch (error) {
           next(error)
      }
